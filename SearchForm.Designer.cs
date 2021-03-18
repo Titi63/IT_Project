9d4e34e2cbaba7640d5fb460eeaad5b61d588c83
anna-2323 <79278381+anna-2323@users.noreply.github.com>
2021-03-18 17:47:58 +0200

    Missing Forms

---
+++ SearchForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace KursovProekt
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cocktailLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.criteriaComboBox = new System.Windows.Forms.ComboBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cocktailLabel
+            // 
+            this.cocktailLabel.AutoSize = true;
+            this.cocktailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cocktailLabel.Location = new System.Drawing.Point(317, 23);
+            this.cocktailLabel.Name = "cocktailLabel";
+            this.cocktailLabel.Size = new System.Drawing.Size(122, 31);
+            this.cocktailLabel.TabIndex = 3;
+            this.cocktailLabel.Text = "Търсене";
+            this.cocktailLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.searchTextBox.Location = new System.Drawing.Point(150, 69);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(313, 26);
+            this.searchTextBox.TabIndex = 4;
+            // 
+            // criteriaComboBox
+            // 
+            this.criteriaComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.criteriaComboBox.FormattingEnabled = true;
+            this.criteriaComboBox.Location = new System.Drawing.Point(469, 67);
+            this.criteriaComboBox.Name = "criteriaComboBox";
+            this.criteriaComboBox.Size = new System.Drawing.Size(108, 28);
+            this.criteriaComboBox.TabIndex = 5;
+            // 
+            // backButton
+            // 
+            this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.backButton.Location = new System.Drawing.Point(150, 381);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(146, 49);
+            this.backButton.TabIndex = 10;
+            this.backButton.Text = "Назад";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.criteriaComboBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.cocktailLabel);
+            this.Name = "SearchForm";
+            this.Text = "Търсене";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cocktailLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ComboBox criteriaComboBox;
+        private System.Windows.Forms.Button backButton;
+    }
+}
